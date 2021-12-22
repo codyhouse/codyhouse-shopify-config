cac8e570b72221815233f20b43addf16cdda6d11
Omar Herri <herri.omar@gmail.com>
2021-12-22 13:38:03 +0100

    Fix errors when trying to install packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 sass.compiler = require('sass-embedded');
 var sassGlob = require('gulp-sass-glob');
 var postcss = require('gulp-postcss');
